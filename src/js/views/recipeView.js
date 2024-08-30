2f74b8cfb3ae28050b47a1dab5ca059529ef3453
Muskan1842 <mush.rpt@gmail.com>
2024-08-30 14:16:45 +0530

    fixing fraction dependancy

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 
 import icons from 'url:../../img/icons.svg'
 import View from './View.js';
-import { Fraction } from 'fractional';
+import Fraction from 'fraction.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
